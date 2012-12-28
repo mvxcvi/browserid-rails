1bdb3a15cdc5db3115f4bbf28f433aec8a2290cd
Greg Look <greg@greg-look.net>
2012-12-28 16:23:45 +0000

    Added more controller base functionality and application configuration sections.

--- lib/browserid-rails.rb
+++ lib/browserid-rails.rb
@@ -1,7 +1,6 @@
-require 'browserid/rails/version'
 require 'browserid/rails/base'
 require 'browserid/rails/helpers'
-require 'browserid/verifier/persona'
+require 'browserid/rails/version'
 
 module BrowserID
   module Rails
@@ -9,9 +8,32 @@ module BrowserID
     # the library methods. The presence of this engine also causes assets to
     # be included when the gem is added as a dependency.
     class Engine < ::Rails::Engine
-      initializer "browserid-rails" do |app|
+      config.before_configuration do
+        BrowserIDConfig = Struct.new :user_model, :email_field, :verifier, :audience
+
+        config.browserid = BrowserIDConfig.new
+        config.browserid.user_model = 'User'
+        config.browserid.email_field = 'email'
+        config.browserid.verifier = :persona
+        # config.browserid.audience should be set by the application
+      end
+
+      initializer "browserid-rails.extend" do |app|
         ActionController::Base.send :include, BrowserID::Rails::Base
         ActionView::Base.send :include, BrowserID::Rails::Helpers
+      end
+
+      config.after_initialize do
+        cfg = config.browserid
+
+        # Replace type symbol with constructed verifier.
+        if cfg.verifier == :persona
+          cfg.verifier = BrowserID::Verifier::Persona.new(cfg.audience)
+        elsif cfg.verifier == :local
+          raise "Local BrowserID verification is not supported yet" # TODO
+        elsif !cfg.verifier.respond_to?(:verify)
+          raise "Unknown BrowserID verifier type #{cfg.verifier}"
+        end
       end
     end
   end
--- lib/browserid/rails/base.rb
+++ lib/browserid/rails/base.rb
@@ -1,18 +1,42 @@
+require 'browserid/verifier/persona'
+
 module BrowserID
   module Rails
     # Public: Base module for inclusion into a controller. This module includes
-    # methods for dealing with BrowserID user authentication. There must be a
-    # `find_user_by_email` method which looks up a user by email address.
+    # methods for dealing with BrowserID user authentication.
     module Base
+
+      ##### INTERNAL METHODS #####
+
       # Internal: Modifies the controller this module is included in to provide
       # authentication-related helper methods
       #
-      # base - The Class the module is being included in.
+      # base - The Class this module is being included in.
       def self.included(base)
         base.send :helper_method, :browserid_email, :current_user, :authenticated?
       end
 
-      private
+      # Internal: Gets the application configuration for this gem.
+      #
+      # Returns the app config structure.
+      def browserid_config
+        ::Rails.application.config.browserid
+      end
+
+
+
+      ##### AUTHENTICATION METHODS #####
+
+      # Public: Verifies the given BrowserID assertion. If successful, this
+      # sets the authenticated email as the logged-in user.
+      #
+      # Returns the authenticated email on success.
+      # Raises an exception on failure.
+      def verify_browserid(assertion)
+        email, issuer = browserid_config.verifier.verify(assertion)
+        logger.info "Verified BrowserID assertion for #{email} issued by #{issuer}"
+        login_browserid email
+      end
 
       # Public: Sets the given email address as the currently-authenticated user.
       # The address is saved in the client's session.
@@ -29,6 +53,10 @@ module BrowserID
         session[:browserid_email] = nil
       end
 
+
+
+      ##### HELPER METHODS #####
+
       # Public: Gets the email address of the currently-authenticated user.
       #
       # Returns the authenticated email address String.
@@ -36,13 +64,23 @@ module BrowserID
         session[:browserid_email]
       end
 
-      # Public: Retrieves the user for the currently-authenticated email address.
-      # This expects a method named `find_user_by_email` which returns the user
-      # for a given email address.
+      # Public: Retrieves the user for the authenticated email address. This
+      # method uses the `browserid.user_model` and `browserid.email_field`
+      # config settings, which default to `User` and `email`.
       #
       # Returns the current authenticated user, or nil if no user exists.
       def current_user
-        @current_user ||= find_user_by_email(browserid_email) if browserid_email
+        if browserid_email.nil?
+          nil
+        elsif @current_user
+          @current_user
+        else
+          config = browserid_config
+          user_model = config.user_model.constantize
+          find_method = "find_by_#{config.email_field}".intern
+
+          @current_user = user_model.send find_method, browserid_email
+        end
       end
 
       # Public: Determines whether the current client is authenticated as a
