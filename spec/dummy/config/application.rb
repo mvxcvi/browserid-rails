45c23920d103b7a8a445742e99d9457aea66746b
Greg Look <greg@greg-look.net>
2012-12-31 05:18:40 +0000

    Add dummy rails app.

---
+++ spec/dummy/config/application.rb
@@ -0,0 +1,18 @@
+require File.expand_path('../boot', __FILE__)
+
+# Pick the frameworks you want:
+#require "active_record/railtie"
+require "action_controller/railtie"
+#require "action_mailer/railtie"
+#require "active_resource/railtie"
+#require "sprockets/railtie"
+
+#Bundler.require
+#require "browserid-rails"
+
+module Dummy
+  class Application < Rails::Application
+    # application configuration goes here
+  end
+end
+
---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '812f71c83c24fe89d77e651a1a83dd46224dd5a4e7d5ee53c841ec12b0ceea408419c8ed2a5d61459cc7b6af21fa639efb928b9d472e134be42177fa3967c479'
